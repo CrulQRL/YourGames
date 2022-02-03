a8b357927785f63d444c44e6c1703d74d98b107a
Faqrul <faqrul.anshurulloh@aruna.com>
2022-02-03 11:56:39 +0700

    image in detail

--- app/src/main/java/com/faqrulans/yourgames/detail/DeveloperDetailFragment.kt
+++ app/src/main/java/com/faqrulans/yourgames/detail/DeveloperDetailFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.faqrulans.yourgames.databinding.FragmentDeveloperDetailBinding
 
 class DeveloperDetailFragment : Fragment() {
 
     private var _binding: FragmentDeveloperDetailBinding? = null
     private val binding get() = _binding!!
+
+    private val args by navArgs<DeveloperDetailFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,15 @@ class DeveloperDetailFragment : Fragment() {
         return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(requireContext())
+            .load(args.developer.imageBackground)
+            .centerCrop()
+            .into(binding.ivDeveloperDetail)
+    }
 
 
     override fun onDestroyView() {
